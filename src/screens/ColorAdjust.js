6a3f018048be13a0d5316e4a557b52ee2a29c344
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-05-23 11:51:29 +0300

    color adjust component further developed with css and variables

--- src/screens/ColorAdjust.js
+++ src/screens/ColorAdjust.js
@@ -1,16 +1,89 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Button, FlatList} from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
 
 function ColorAdjust() {
-  return (
-    <Text style={styles.text}>ColorAdjust</Text>
-  )
+    let [colorOfBox, setColorOfBox] = useState("pink");
+    let[redColor, setRedColor] = useState("rgb()")
+    
+    return (
+        <>
+            <View>
+                <TouchableOpacity
+                    style={styles.buttonsRed} 
+                    > RED ++ </TouchableOpacity>
+                <TouchableOpacity 
+                    style={styles.buttonsRed}
+                    > RED -- </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity 
+                    style={styles.buttonsGreen}
+                    >GREEN ++</TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.buttonsGreen} 
+                    >GREEN --</TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity 
+                    style={styles.buttonsBlue}
+                    >BLUE ++</TouchableOpacity>
+                <TouchableOpacity 
+                    style={styles.buttonsBlue}
+                    >BLUE --</TouchableOpacity>
+            </View>
+            <View 
+                style={{
+                    borderRadius : 8,
+                    border: "2px solid #0000FF",
+                    height: 200,
+                    width: 200,
+                    backgroundColor: colorOfBox,
+                    textAlign: "center",
+                    margin: "auto",
+                }}
+            >
+            </View>
+        </>
+    )
 }
 
 const styles = StyleSheet.create({
-    text: {
-        backgroundColor: "blue",
-    }
+    buttonsRed: {
+        backgroundColor: 'red',
+        border: "1px solid white",
+        textAlign: "center",
+        color: "white",
+        padding: 5,
+        fontSize: 25,
+        fontFamily: "Verdana",
+        fontWeight: "bold",
+    },
+    buttonsGreen: {
+        backgroundColor: 'green',
+        border: "1px solid white",
+        textAlign: "center",
+        color: "white",
+        padding: 5,
+        fontSize: 25,
+        fontFamily: "Verdana",
+        fontWeight: "bold",
+    },
+    buttonsBlue: {
+        backgroundColor: 'blue',
+        border: "1px solid white",
+        textAlign: "center",
+        color: "white",
+        padding: 5,
+        fontSize: 25,
+        fontFamily: "Verdana",
+        fontWeight: "bold",
+    },
+    colorBox: {
+        borderRadius : 8,
+        border: "2px solid #0000FF",
+        height: 100,
+        width: 100,
+    },
 })
 
-export default ColorAdjust
+export default ColorAdjust;
