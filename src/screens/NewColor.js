300fff4b105e90d8431350062190f76a829ddec2
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-07-03 22:45:07 +0300

    color screen is finished

--- src/screens/NewColor.js
+++ src/screens/NewColor.js
@@ -2,13 +2,45 @@ import React, { useState } from "react";
 import { Text, StyleSheet, View, FlatList, Button, TouchableOpacity } from "react-native";
 
 function NewColor() {
-  return (
-    <View style={styles.boxContainer}>
-        <TouchableOpacity style={styles.buttonStyle} onPress={() => {console.log("veververv")}}>
-            <Text style={styles.textStyle1}>ADD COLORBOX</Text>
-        </TouchableOpacity>
-    </View>
-  )
+
+    let [colorArray, setColorArray] = useState([null]);
+
+    const addColor = () => {
+        let num1 = Math.floor(Math.random() * 256);
+        let num2 = Math.floor(Math.random() * 256);
+        let num3 = Math.floor(Math.random() * 256);
+      setColorArray([...colorArray, `rgb(${num1}, ${num2}, ${num3})`]);
+    }
+
+    function getRandomInt(min, max) {
+        // min is inclusive, max is exclusive
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+      }
+
+    return (
+        <View style={styles.boxContainer}>
+
+            <TouchableOpacity style={styles.buttonStyle} onPress={() => addColor()}>
+                <Text style={styles.textStyle1}>ADD COLORBOX</Text>
+            </TouchableOpacity>
+            <Text>{colorArray}</Text>
+            <FlatList 
+                data={colorArray}
+                renderItem={({item}) => {
+                    return(
+                        <View style={{
+                            marginBottom: 5,
+                            width: 80,
+                            height: 80,
+                            backgroundColor: item}}></View>
+                    )
+                }}
+            />
+
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -23,8 +55,9 @@ const styles = StyleSheet.create({
         
     },
     boxStyle: {
-        width: 200,
-        height: 200,
+        width: 80,
+        height: 80,
+        marginBottom: 5,
     },
     boxContainer: {
         justifyContent: "center",
