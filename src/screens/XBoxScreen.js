b18be6d23ffb6869d515dd7c225bbd63d5367252
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-07-13 21:03:49 +0300

    box css screenss finished

--- src/screens/XBoxScreen.js
+++ src/screens/XBoxScreen.js
@@ -4,49 +4,71 @@ import { Text, StyleSheet, View, FlatList, Button, TouchableOpacity } from "reac
 function XBoxScreen() {
   return (
     <View style={styles.boxContainer}>
-        <Text style={styles.textStyle2}>Child 1</Text>
-        <Text style={styles.textStyle2}>Child 2</Text>
-        <Text style={styles.textStyle2}>Child 3</Text>
+        <View style={styles.box1}>Child 1</View>
+        <View style={styles.box2}>Child 2</View>
+        <View style={styles.box3}>Child 3</View>
+
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    textStyle1: {
-        fontFamily: "Trebuchet MS",
-        color: "#ffffff",
-        marginVertical: 7,
-        margin: "auto",
-        fontSize: 18,
-        fontWeight: 800,
-        
+    boxContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        borderWidth: 2,
+        borderColor: "black",
+    },
+    box1: {
+        height: 80,
+        width: 80,
+        backgroundColor: "blue",
+    },
+    box2: {
+        height: 80,
+        width: 80,
+        backgroundColor: "green",
+        top: 80,
+    },
+    box3: {
+        height: 80,
+        width: 80,
+        backgroundColor: "yellow",
+    },
+
+    boxContainer2: {
+        flexDirection: "row",
+        marginTop: 5,
+        borderWidth: 4,
+        borderColor: "black",
     },
     textStyle2: {
         fontFamily: "Trebuchet MS",
+        fontSize: 18,
+        fontWeight: 800,
+        height: 200,
+        borderWidth: 3,
+        borderColor: "blue",
+    },
+    textStyle3: {
+        fontFamily: "Trebuchet MS",
+        fontSize: 18,
+        fontWeight: 800,
+        height: 200,
+        borderWidth: 3,
+        borderColor: "blue",
+        flex: 1,
+        alignSelf: "flex-start",
+    },
+    textStyle1: {
+        fontFamily: "Trebuchet MS",
         marginVertical: 7,
         margin: "auto",
         fontSize: 18,
         fontWeight: 800,
+        borderWidth: 3,
+        borderColor: "blue",
     },
-    boxStyle: {
-        width: 80,
-        height: 80,
-        marginBottom: 5,
-    },
-    boxContainer: {
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    buttonStyle: {
-        textAlign: "center",
-        width: 200,
-        height: 40,
-        margin: "auto",
-        backgroundColor: "#3a7411",
-        borderRadius: 8,
-        borderColor: "#000000",
-        borderWidth: 2,
-    }
 })
 
 export default XBoxScreen
